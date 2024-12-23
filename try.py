769be01be7790a12426ba5b47ef0c0db8fc72270
samyareh <sam@test.com>
2024-12-23 12:44:57 +0300

    Revert "m"
    This reverts commit e351b28949077593fcdcbc2f14aeef3627ee0c6a.

--- try.py
+++ try.py
@@ -1,13 +1,5 @@
 print("this is a test file")
-<<<<<<< HEAD
 print("my lif")
 print("nigga")
 print("test4")
 print("test5")
-=======
-<<<<<<< HEAD
-print("test2")
-=======
-print("my lif")
->>>>>>> br2
->>>>>>> parent of a75ecb6 (initial-d)
