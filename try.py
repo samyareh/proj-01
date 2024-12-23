e351b28949077593fcdcbc2f14aeef3627ee0c6a
samyareh <sam@test.com>
2024-12-23 12:42:35 +0300

    m

--- try.py
+++ try.py
@@ -1,5 +1,13 @@
 print("this is a test file")
+<<<<<<< HEAD
 print("my lif")
 print("nigga")
 print("test4")
 print("test5")
+=======
+<<<<<<< HEAD
+print("test2")
+=======
+print("my lif")
+>>>>>>> br2
+>>>>>>> parent of a75ecb6 (initial-d)
